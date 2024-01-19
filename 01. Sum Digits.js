ec909c86df26c5d25df4a1898f3b742ced485ec1
Miroslav Tzarev <mtzarev@gmail.com>
2024-01-19 17:23:51 +0200

    Add files via upload

---
+++ 01. Sum Digits.js
@@ -0,0 +1,14 @@
+function sumDigits(num) {
+    num = num.toString()
+    let sum = 0
+    for (let i = 0; i < num.length; i++) {
+
+        sum += Number(num[i])
+
+        // console.log(sum);
+    }
+    console.log(sum);
+}
+//sumDigits(245678);
+//sumDigits(97561);
+sumDigits(543)
